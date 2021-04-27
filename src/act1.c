90d9d152e09a45791fb2bce332eb87b4bbe64182
amol.kerkar <amolkerkar2@gmail.com>
2021-04-27 14:51:03 +0530

    updated act1

--- src/act1.c
+++ src/act1.c
@@ -1,1 +1,22 @@
 //act 1 code
+#include "function.h"
+
+int firstact(void)
+{
+    DDRB = DDRB&0b11111101;//fd
+    DDRC = DDRC|0b01000000;//40
+    DDRB = DDRB&0b11111110;//fe
+
+
+    while(1){
+        if((PINB & 0b00000010)||(PINB & 0b0000001)==1)
+        {//02 & 01
+            PORTC = PORTC&0b10111111;//bf
+        }
+        else
+        {
+            PORTC = PORTC|0b01000000;//40
+        }
+    }
+
+}
