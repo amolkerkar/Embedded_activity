6087a5645536ffe97f2a756ff588f3e17e7ba5f5
amol.kerkar <amolkerkar2@gmail.com>
2021-04-27 13:57:02 +0530

    added doxygen comments

--- inc/function.h
+++ inc/function.h
@@ -1,7 +1,17 @@
 #include <avr/io.h>
+
 #ifndef __FUNCTION_H__
 #define __FUNCTION_H__
+/**
+ * @brief functions carried out are void
+ *  
+ */
+
 
 int first_activity(void);
+/**
+ * @brief this is the fuction of first activity "automatic seat heating " in the ltts stepin project. 
+ * @brief If both the switches are on, the LED must be on, otherwise off.
+ */
 
 #endif 
