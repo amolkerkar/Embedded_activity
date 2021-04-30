3d970a537b2801b37fbe6d149bc89930645e29c7
264877 <81116875+amolkerkar@users.noreply.github.com>
2021-05-01 01:10:50 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,24 @@
+#include <avr/io.h>
+#include "ACTIVITY_1.h"
+#include "ACTIVITY_2.h"
+#include "ACTIVITY_3.h"
+#include "ACTIVITY_3.h"
 
+/** \brief main function where code execution
+ *
+ * \param void
+ * \return int
+ *
+ */
 
 int main(void)
 {
+    while(1)
+    {
     firstact();
     secondact();
     thirdact();
     finalact();
-
-    return 0;
+    }
+return 0;
 }
